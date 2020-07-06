06622f395920c83a7384364f28f3ed6a1e5caf76
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-07-05 23:35:27 -0700

    added scramble function

--- temere.py
+++ temere.py
@@ -15,14 +15,29 @@ def generate_temere(length=32, uppercase=True, lowercase=True, numbers=True):
     
     return ''.join(random.choice(character_set) for i in range(length))
 
+def scramble(word):
 
-user_letter = input("Please select a letter: ")
+    jumble = ''
+
+    while word:
+        position = random.randrange(len(word))
+        jumble += word[position]
+        word = word[:position] + word[(position + 1):]
+    
+    return jumble
+
+
+# user_letter = input("Please select a letter: ")
+user_word = input("Please enter a word: ")
 
 print("Press enter to generate...")
 
 while True:
     if input() == '':
-        result = generate_temere(4, True, False, False)
-        print(user_letter + result)
+        # result = generate_temere(4, True, False, False)
+        # print(user_letter + result)
+
+        result = scramble(user_word)
+        print(result)
     else:
         break
